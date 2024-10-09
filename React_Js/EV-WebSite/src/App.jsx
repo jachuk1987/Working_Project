2b373009d0d9ad1823b54cb85ec5b2a255e700ba
jeswanth <jachu.k1212@gmail.com>
2024-10-09 22:24:07 +0530

    ADD

--- React_Js/EV-WebSite/src/App.jsx
+++ React_Js/EV-WebSite/src/App.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 
 const App = () => {
+  let heroData = [
+    {text1:"Dive into",text2:"what you love"},
+    {text1:"Indulge",text2:"your passions"},
+    {text1:"Give in to",text2:"your passions"},
+  ]
   return (
     <div>
 

6507f237b7bc84913ef9e41dfa0daef176ba2c30
jeswanth <jachu.k1212@gmail.com>
2024-10-10 10:15:46 +0530

    ADD

--- React_Js/EV-WebSite/src/Components/Background/Background.jsx
+++ React_Js/EV-WebSite/src/Components/Background/Background.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import "../Background/Background.css";
+import video1 from "../../assets/video1.mp4";
+import image1 from "../../assets/image1.png";
+import image2 from "../../assets/image2.png";
+import image3 from "../../assets/image3.png";
 
-const Background = () => {
+const Background = ({playStatus,heroCount}) => {
+
+    if (playStatus) {
+        return (
+            <video className="background" autoPlay loop muted>
+                <source src={video1} type="video/mp4"/>
+            </video>
+        )
+    }
+    else if(heroCount===1)
     return (
         <div>
 

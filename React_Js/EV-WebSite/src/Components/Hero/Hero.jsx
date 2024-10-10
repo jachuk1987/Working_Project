ac581f7f220ca6a6c286ea993308db240dfc1c5e
jeswanth <jachu.k1212@gmail.com>
2024-10-10 11:23:38 +0530

    ADD

--- React_Js/EV-WebSite/src/Components/Hero/Hero.jsx
+++ React_Js/EV-WebSite/src/Components/Hero/Hero.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import "../Hero/Hero.css";
+import arrow_btn from "../../assets/arrow_btn.png";
+import play_icon from "../../assets/play-icon.png";
+import pause_icon from "../../assets/pause_icon.png";
 
-const Hero = () => {
+const Hero = ({heroData,setHeroCount,heroCount,setPlayStatus,playStatus}) => {
     return (
-        <div>
+        <div className="hero">
 
         </div>
     )

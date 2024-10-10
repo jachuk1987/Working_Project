1312dec6b4c894a708ec7a9d4964664b5402e762
jeswanth <jachu.k1212@gmail.com>
2024-10-10 12:07:26 +0530

    ADD

--- React_Js/EV-WebSite/src/Components/Hero/Hero.jsx
+++ React_Js/EV-WebSite/src/Components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../Hero/Hero.css";
 import arrow_btn from "../../assets/arrow_btn.png";
-import play_icon from "../../assets/play-icon.png";
+import play_icon from "../../assets/play_icon.png";
 import pause_icon from "../../assets/pause_icon.png";
 
 const Hero = ({heroData,setHeroCount,heroCount,setPlayStatus,playStatus}) => {
@@ -13,7 +13,17 @@ const Hero = ({heroData,setHeroCount,heroCount,setPlayStatus,playStatus}) => {
             </div>
             <div className="hero-explore">
                 <p>Explore the features</p>
-                <img src="" alt="" />
+                <img src={arrow_btn} alt="" />
+            </div>
+            <div className="hero-dot-play">
+                <ul className="hero-dots">
+                    <li onClick={()=>setHeroCount(0)} className={heroCount===0?"hero-dot orange":"hero-dot"}></li>
+                    <li onClick={()=>setHeroCount(1)} className={heroCount===1?"hero-dot orange":"hero-dot"}></li>
+                    <li onClick={()=>setHeroCount(2)} className={heroCount===2?"hero-dot orange":"hero-dot"}></li>
+                </ul>
+                <div className="hero-play">
+                    <img onClick={()=>setPlayStatus(!playStatus)} src={playStatus?pause_icon:play_icon} alt="" />
+                </div>
             </div>
         </div>
     )

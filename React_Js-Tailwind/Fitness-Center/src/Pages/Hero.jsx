29cd9b3490fbecaa021f774a3d05e68531ae217c
jeswanth <jachu.k1212@gmail.com>
2024-10-12 09:55:51 +0530

    Hero

---
+++ React_Js-Tailwind/Fitness-Center/src/Pages/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+const Hero = () => {
+    return (
+        <>
+            <div className="w-full h-auto">
+
+            </div>
+        </>
+    )
+}
+
+export default Hero;

a23feae3483d0c6a38b5737d948b8d341a1ef574
jeswanth <jachu.k1212@gmail.com>
2024-10-12 09:21:37 +0530

    ADD

--- React_Js-Tailwind/Fitness-Center/src/App.jsx
+++ React_Js-Tailwind/Fitness-Center/src/App.jsx
@@ -1,1 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Pages/Home";
+
+const App = () => {
+    return (
+        <div className="w-full min-h-screen h-auto bg-[#171717]">
+            <Router>
+                <Navbar />
+                <Home />
+            </Router>
+        </div>
+    )
+}
+
+export default App;

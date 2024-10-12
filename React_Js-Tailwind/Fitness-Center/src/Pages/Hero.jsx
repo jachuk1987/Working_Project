16eb87509454997a4f1ac76d77240e1c8f718932
jeswanth <jachu.k1212@gmail.com>
2024-10-12 16:55:52 +0530

    Hero

--- React_Js-Tailwind/Fitness-Center/src/Pages/Hero.jsx
+++ React_Js-Tailwind/Fitness-Center/src/Pages/Hero.jsx
@@ -7,7 +7,7 @@ import "swiper/css/navigation";
 import { Autoplay, Navigation } from "swiper/modules";
 const Hero = () => {
 
-    const swiperData: [
+    const swiperData = [
         {
             id: 1,
             bgImg: "https://source.unsplash.com/random/?fitness",
@@ -57,14 +57,14 @@ const Hero = () => {
                         Slide 1
                     </SwiperSlide>
 
-                    <SwiperSlide>Slide 2</SwiperSlide>
+                    {/* <SwiperSlide>Slide 2</SwiperSlide>
                     <SwiperSlide>Slide 3</SwiperSlide>
                     <SwiperSlide>Slide 4</SwiperSlide>
                     <SwiperSlide>Slide 5</SwiperSlide>
                     <SwiperSlide>Slide 6</SwiperSlide>
                     <SwiperSlide>Slide 7</SwiperSlide>
                     <SwiperSlide>Slide 8</SwiperSlide>
-                    <SwiperSlide>Slide 9</SwiperSlide>
+                    <SwiperSlide>Slide 9</SwiperSlide> */}
                 </Swiper>
             </div>
         </>

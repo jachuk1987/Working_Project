e8c26fbdd070c88872ebbc90020051a853551a98
jeswanth <jachu.k1212@gmail.com>
2024-10-13 10:20:25 +0530

    Hero

--- React_Js-Tailwind/Fitness-Center/src/Pages/Hero.jsx
+++ React_Js-Tailwind/Fitness-Center/src/Pages/Hero.jsx
@@ -47,17 +47,20 @@ const Hero = () => {
                     <SwiperSlide
                         className="w-full h-[70vh]"
                         style={{
-                            backgroundImage: "url('./images/hero-1.jpg')",
+                            backgroundImage: "url('https://source.unsplash.com/random/?fitness')",
                             backgroundSize: "cover",
                             backgroundPosition: "center",
                             backgroundRepeat: "no-repeat"
                         }}
                     >
-                        <div className="w-full h-full bg-black/80 absoluter top-0 left-0 -z-10"></div>
+                        <div className="w-full h-full bg-black/80 absolute top-0 left-0 -z-10"></div>
                         <div className="w-full h-full flex items-center justify-center flex-col z-10 lg:px-24 md:px-16 sm:px-6 px-4">
                             <h5 className="lg:text-4x1 md:text-3x1 sm:text-2x1 text-2x1 text-indigo-400 font-semibold mb-2 uppercase">
                             Keep your body
                             </h5>
+                            <h1 className="lg:text-8x1 md:text-7x1 sm:text-5x1 text-5x1 font-bold text-white mb-4 uppercase">
+                                Burning
+                            </h1>
                         </div>
                     </SwiperSlide>
                 </Swiper>

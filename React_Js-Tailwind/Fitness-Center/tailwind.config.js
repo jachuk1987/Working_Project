a6fe2dbfdbbb1e20e8af1aabae15de28f62b53b9
jeswanth <jachu.k1212@gmail.com>
2024-10-14 11:18:19 +0530

    ADD

--- React_Js-Tailwind/Fitness-Center/tailwind.config.js
+++ React_Js-Tailwind/Fitness-Center/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src//*.{js,jsx,ts,tsx}', // Includes all JS, JSX, TS, and TSX files in the src directory
+    './public/index.html',
   ],
   theme: {
     extend: {},

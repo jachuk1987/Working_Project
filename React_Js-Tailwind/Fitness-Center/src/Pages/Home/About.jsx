a101f44e87fc9e0fe02f973b028743688b30be88
jeswanth <jachu.k1212@gmail.com>
2024-10-17 10:13:07 +0530

    About

--- React_Js-Tailwind/Fitness-Center/src/Pages/Home/About.jsx
+++ React_Js-Tailwind/Fitness-Center/src/Pages/Home/About.jsx
@@ -3,17 +3,17 @@ import React from "react";
 const About = () => {
     return (
         <>
-        <div className="w-full h-auto flex items-center justify-between gap-7 lg:py-16
+            <div className="w-full h-auto flex items-center justify-between gap-7 lg:py-16
         md:py-14 sm:py-12 py-10 lg:px-16 sm:px-6 px-4 flex-wrap-reverse">
-            <div className="lg:w-[38%] md:w-full sm:w-full h-auto relative
+                <div className="lg:w-[38%] md:w-full sm:w-full h-auto relative
             overflow-hidden rounded-lg">
-                <img 
-                src="https://source.unsplash.com/random/?gym" 
-                alt="About image" 
-                className="w-full lg:h-[70vh] md:h-[68vh] sm:h-[60vh] h-[55vh] object-cover"
-                />
+                    <img
+                        src="https://source.unsplash.com/random/?gym"
+                        alt="About image"
+                        className="w-full lg:h-[70vh] md:h-[68vh] sm:h-[60vh] h-[55vh] object-cover"
+                    />
+                </div>
             </div>
-        </div>
         </>
     )
 }

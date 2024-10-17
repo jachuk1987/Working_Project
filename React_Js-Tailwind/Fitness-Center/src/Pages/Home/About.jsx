de1cb39f87f57cc36649a3cf0ee460faaa892841
jeswanth <jachu.k1212@gmail.com>
2024-10-17 10:22:35 +0530

    About

--- React_Js-Tailwind/Fitness-Center/src/Pages/Home/About.jsx
+++ React_Js-Tailwind/Fitness-Center/src/Pages/Home/About.jsx
@@ -15,7 +15,11 @@ const About = () => {
                 </div>
                 <div className="lg:w-[50%] md:w-full sm:w-full w-full h-auto lg:text-start
                 md:text-start sm:text-start text-center">
-
+                    <h6 className="text-lg font-medium text-gray-200 flex items-center
+                    lg:justify-start md:justify-start sm:justify-center justify-center gap-x--2 mb-3">
+                        <Dumbbell classname="w-4 h-4 -rotate-45 text-indigo-600"/>
+                        About Us
+                    </h6>
                 </div>
             </div>
         </>

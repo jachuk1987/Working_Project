555069e298272013813b86f91302fe785132c46d
jeswanth <jachu.k1212@gmail.com>
2024-10-17 08:07:41 +0530

    About

--- React_Js-Tailwind/Fitness-Center/src/Pages/Home/About.jsx
+++ React_Js-Tailwind/Fitness-Center/src/Pages/Home/About.jsx
@@ -5,7 +5,10 @@ const About = () => {
         <>
         <div className="w-full h-auto flex items-center justify-between gap-7 lg:py-16
         md:py-14 sm:py-12 py-10 lg:px-16 sm:px-6 px-4 flex-wrap-reverse">
-            <div className="lg"></div>
+            <div className="lg:w-[38%] md:w-full sm:w-full h-auto relative
+            overflow-hidden">
+                
+            </div>
         </div>
         </>
     )

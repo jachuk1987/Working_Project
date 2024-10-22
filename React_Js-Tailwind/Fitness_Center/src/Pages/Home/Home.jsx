0f71b69ad3d01580e2f9fafa0ce1e36bf94dd7cf
jeswanth <jachu.k1212@gmail.com>
2024-10-22 13:17:34 +0530

    GYM_BHI

--- React_Js-Tailwind/Fitness_Center/src/Pages/Home/Home.jsx
+++ React_Js-Tailwind/Fitness_Center/src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "./Hero";
 import About from "./About";
+import BMI from "./BMI";
 
 const Home = () => {
     return (
@@ -8,6 +9,7 @@ const Home = () => {
             <div className="w-full h-auto overflow-hidden">
                 <Hero />
                 <About />
+                <BMI />
             </div>
         </>
     )

90b64155e2e843193338eba57a97b6fcc043cd14
jeswanth <jachu.k1212@gmail.com>
2024-10-22 13:22:04 +0530

    Gym

--- React_Js-Tailwind/Fitness_Center/src/Pages/Home/BMI.jsx
+++ React_Js-Tailwind/Fitness_Center/src/Pages/Home/BMI.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const BMI = () => {
     return (
         <>
-            <div className="w-full h-auto flex">
+            <div className="w-full h-auto flex items-center lg:py-16 md:py-14 sm:py-12 py-10
+            lg:px-24 md:px-16 sm:px-6 px-4">
 
             </div>
         </>

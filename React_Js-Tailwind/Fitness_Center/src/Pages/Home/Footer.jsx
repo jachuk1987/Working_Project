f707c1a8e09b97daa0593d3039aa5647ce6fe8a4
jeswanth <jachu.k1212@gmail.com>
2024-10-24 07:42:05 +0530

    Footer

--- React_Js-Tailwind/Fitness_Center/src/Pages/Home/Footer.jsx
+++ React_Js-Tailwind/Fitness_Center/src/Pages/Home/Footer.jsx
@@ -9,12 +9,23 @@ const Footer = () => {
             flex-copy-16 lg:px-24 md:px-16 sm:px-6 px-4">
                 <div className="w-full h-auto flex items-start lg:justify-between
                 md:justify-between sm:justify-center justify-center gap-5 mb-10 flex-wrap">
-                    <Link className="text-3xl text-indigo-600 font-semibold tracking-[0.1rem] flex 
-                    items-end gap-x-1 relative">
+                    <div className="lg:w-[30%] md:w-[30%] sm:w-full w-full h-auto p-2 flex 
+                    lg:items-start md:items-start sm:items-center items-center flex-col 
+                    justify-center">
+                      <Link className="w-fit h-autotext-3xl text-indigo-600 font-semibold tracking-
+                      [0.1rem] flex items-end gap-x-1 relative">
                         G<span className="text-xl font-bold text-gray-300">Y</span>M
                         <Dumbbell className="w-5 h-4 text-indigo-600 -rotate-45 absolute top-0 left-
                         [46%] translate-x-[-50%]" />
                     </Link>
+                    <p className="text-base text-gray-500 font-normal mb-4 lg:text-start 
+                    md:text-start sm:text-center text-center">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit 
+                        quae quasi vel, qui delectus voluptate? Maiores aliquid, at sint 
+                        itaque neque et velit quas in!
+                    </p>
+                    </div>
+                    
                 </div>
             </div>
         </>

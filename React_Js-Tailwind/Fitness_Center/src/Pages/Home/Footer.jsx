22eb3a92aa60a044679ee3e66f4481706b968d6a
jeswanth <jachu.k1212@gmail.com>
2024-10-24 11:09:40 +0530

    GYM_Finish

--- React_Js-Tailwind/Fitness_Center/src/Pages/Home/Footer.jsx
+++ React_Js-Tailwind/Fitness_Center/src/Pages/Home/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Dumbbell, MailOpen, MapPin, Phone } from "lucide-react";
+import { Dumbbell, Facebook, Instagram, MailOpen, MapPin, Phone, Youtube } from "lucide-react";
 
 const Footer = () => {
     return (
@@ -51,11 +51,26 @@ const Footer = () => {
                         <div className="lg:w-[30%] md:w-[48%] sm:w-[48%] w-full h-auto flex 
                         items-center flex-col justify-center">
                             <h2 className="text-lg text-gray-200 font-medium mb-4">
-                                Services
+                                Quick Link
                             </h2>
                             <div className="w-full h-auto">
                                 <ul className="w-full h-auto space-y-2">
-                                    li.w-full.h-auto.text-gray-400.hover:text-indigo-600.ease-out.duration-300
+                                    <li className="w-full h-auto text-gray-400 hover:text-indigo-600 
+                                    ease-out duration-300 cursor-pointer text-center">
+                                        Home
+                                    </li>
+                                    <li className="w-full h-auto text-gray-400 hover:text-indigo-600 
+                                    ease-out duration-300 cursor-pointer text-center">
+                                        About
+                                    </li>
+                                    <li className="w-full h-auto text-gray-400 hover:text-indigo-600 
+                                    ease-out duration-300 cursor-pointer text-center">
+                                        Pricing Plan
+                                    </li>
+                                    <li className="w-full h-auto text-gray-400 hover:text-indigo-600 
+                                    ease-out duration-300 cursor-pointer text-center">
+                                        Contact
+                                    </li>
                                 </ul>
                             </div>
                         </div>
@@ -64,6 +79,39 @@ const Footer = () => {
                             <h2 className="text-lg text-gray-200 font-medium mb-4">
                                 Quick Link
                             </h2>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-full h-[1.5px] flex items-center lg:bg-gray-900 md:bg-gray-900
+                sm:bg-gray-900 sm:bg-transparent relative">
+                    <div className="w-full h-auto flex items-center lg:justify-between md:justify-between 
+                    sm:justify-center justify-center absolute -top-4 left-[50%]
+                    translate-x-[-50%] flex-wrap-reverse gap-y-4">
+                        <p className="text-sm text-gray-400 font-normal lg:bg-gray-900 md:bg-gray-900 
+                        sm:bg-transparent bg-transparent py-1 px-4 rounded-md">
+                            @2024 G-Tech. Allrights reserved.
+                        </p>
+                        <div className="w-auto h-auto flex items-center justify-center">
+                            <ul className="w-full h-auto flex items-center justify-center gap-x-4">
+                                <li className="w-8 h-8 cursor-pointer  flex items-center justify-center
+                                bg-indigo-900 py-1 px-4 rounded-md text-white hover:text-white ease-out duration-700">
+                                    <a href="/" target="_blank">
+                                        <Facebook className="w-5 h-5"/>
+                                    </a>
+                                </li>
+                                <li className="w-8 h-8 cursor-pointer  flex items-center justify-center
+                                bg-indigo-900 py-1 px-4 rounded-md text-white hover:text-white ease-out duration-700">
+                                    <a href="/" target="_blank">
+                                        <Instagram className="w-5 h-5"/>
+                                    </a>
+                                </li>
+                                <li className="w-8 h-8 cursor-pointer  flex items-center justify-center
+                                bg-indigo-900 py-1 px-4 rounded-md text-white hover:text-white ease-out duration-700">
+                                    <a href="/" target="_blank">
+                                        <Youtube className="w-5 h-5"/>
+                                    </a>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>

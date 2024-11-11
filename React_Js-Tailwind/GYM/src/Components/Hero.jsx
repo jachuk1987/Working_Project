c6ae26b22ed0ba7662c9e0b34258490343d97fdd
jeswanth <jachu.k1212@gmail.com>
2024-11-11 10:32:05 +0530

    GYM

--- React_Js-Tailwind/GYM/src/Components/Hero.jsx
+++ React_Js-Tailwind/GYM/src/Components/Hero.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import herobanner from "../assets/herobanner.jpg";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Hero = () => {
+
+    useEffect(()=> {
+        AOS.init({
+            offset: 200,
+            duration: 500,
+            easing: "ease-in-sine",
+        });
+    },[])
+
     return (
-        <div>
-            Hero
-        </div>
+        <section>
+            
+        </section>
     )
 }
 

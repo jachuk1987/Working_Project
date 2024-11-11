7c9989d9ab595c1f0ae0ebf3c3e2e52634149495
jeswanth <jachu.k1212@gmail.com>
2024-11-11 09:46:28 +0530

    GYM

--- React_Js-Tailwind/GYM/src/App.jsx
+++ React_Js-Tailwind/GYM/src/App.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
 import About from "./Components/About";
+import Services from "./Components/Services";
+import Teams from "./Components/Teams";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
@@ -9,6 +13,10 @@ const App = () => {
       <Header />
       <Hero />
       <About />
+      <Services />
+      <Teams />
+      <Contact />
+      <Footer />
     </>
   )
 }

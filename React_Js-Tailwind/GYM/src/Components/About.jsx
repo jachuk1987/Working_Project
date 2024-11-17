d5cf9f7c6cf798685a658a47b36c1baec83c0c48
jeswanth <jachu.k1212@gmail.com>
2024-11-17 10:55:23 +0530

    GYM_Working

--- React_Js-Tailwind/GYM/src/Components/About.jsx
+++ React_Js-Tailwind/GYM/src/Components/About.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import aboutimg from "../assets/about.jpg";
 import { FaCheck } from "react-icons/fa6";
 import { FaQuoteLeft } from "react-icons/fa";
 import banner from "../assets/banner.jpg";
-import Aos from "aos";
+import AOS from "aos";
 import 'aos/dist/aos.css'
 
 const About = () => {
+
+    useEffect(() => {
+        AOS.init({
+            offset: 200,
+            duration: 500,
+            easing: "ease-in-sine",
+        });
+    }, [])
+
     return (
         <>
             <section>

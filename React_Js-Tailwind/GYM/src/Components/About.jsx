f04c61f57b7c6f20887e16e9814f42dd34604189
jeswanth <jachu.k1212@gmail.com>
2024-11-17 10:51:41 +0530

    GYM_New-Working

--- React_Js-Tailwind/GYM/src/Components/About.jsx
+++ React_Js-Tailwind/GYM/src/Components/About.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import aboutimg from "../assets/about.jpg";
+import { FaCheck } from "react-icons/fa6";
+import { FaQuoteLeft } from "react-icons/fa";
+import banner from "../assets/banner.jpg";
+import Aos from "aos";
+import 'aos/dist/aos.css'
 
 const About = () => {
     return (
-        <div>
-            About
-        </div>
+        <>
+            <section>
+
+            </section>
+        </>
     )
 }
 

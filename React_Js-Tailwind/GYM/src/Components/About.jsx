c348e0fc935149edfe6747a14e7ee2204e7a608a
jeswanth <jachu.k1212@gmail.com>
2024-11-18 08:34:26 +0530

    GYM_Working

--- React_Js-Tailwind/GYM/src/Components/About.jsx
+++ React_Js-Tailwind/GYM/src/Components/About.jsx
@@ -19,10 +19,20 @@ const About = () => {
     return (
         <>
             <section id="about" className="w-[75%] mx-auto md:py-20 py-10 flex md:flex-row flex-col justify-between items-start gap-20">
-                <div className="md:w-[50%] w-full flex-col justify-center items-start gap-6">
+                <div className="md:w-[50%] w-full flex flex-col justify-center items-start gap-6">
                     <h1 data-aos="zoom-in" data-aos-delay="" className="text-red-600 md:text-7x1 text-6x1 font-semibold">JOIN TODAY</h1>
                     <p data-aos="zoom-in" data-aos-delay="200" className="text-lg text-slate-600 text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto voluptate repellat, nam reiciendis nostrum sint.</p>
                     <button data-aos="zoom-in" data-aos-delay="600" className="px-8 py-4 bg-blue-900 hover:bg-red-500 text-white font-semibold">BOOK NOW</button>
+                    <div data-aos="zoom-in" data-aos-delay="800" id="list-infos" className="flex md:flex-row flex-col justify-center md:items-center items-start gap-6 md:pr-10">
+                        <div>
+                            <ul className="flex flex-col justify-center items-start gap-2">
+                                <li className="flex justify-center items-center gap-2"><span><FaCheck /></span></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </section>
         </>

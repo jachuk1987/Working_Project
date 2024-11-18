ba9f180336229b602ab9ccd020d38f7ef80b35fb
jeswanth <jachu.k1212@gmail.com>
2024-11-18 08:00:24 +0530

    ADDED

--- React_Js-Tailwind/GYM/src/Components/About.jsx
+++ React_Js-Tailwind/GYM/src/Components/About.jsx
@@ -20,7 +20,8 @@ const About = () => {
         <>
             <section id="about" className="w-[75%] mx-auto md:py-20 py-10 flex md:flex-row flex-col justify-between items-start gap-20">
                 <div className="md:w-[50%] w-full flex-col justify-center items-start gap-6">
-                    <h1 data-aos="zoom-in" data-aos-delay="" className="text-red-600 md:text-7x1 text-6x1">JOIN TODAY</h1>
+                    <h1 data-aos="zoom-in" data-aos-delay="" className="text-red-600 md:text-7x1 text-6x1 font-semibold">JOIN TODAY</h1>
+                    <p data-aos="zoom-in" data-aos-delay="200" className="text-lg text-slate-600 text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto voluptate repellat, nam reiciendis nostrum sint.</p>
                 </div>
             </section>
         </>

b0db02d154d874bd9909e0ba87d3e349e83a8dd0
jeswanth <jachu.k1212@gmail.com>
2024-11-19 08:09:51 +0530

    GYM_Working

--- React_Js-Tailwind/GYM/src/Components/Services.jsx
+++ React_Js-Tailwind/GYM/src/Components/Services.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import phil1 from "../assets/phi1.png";
 import phil2 from "../assets/phil2.png";
+import phil3 from "../assets/phil3.png";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Services = () => {
+
+    useEffect(() => {
+        AOS.init({
+            offset: 200,
+            duration: 500,
+            easing: "ease-in-sine",
+        });
+    }, [])
+
     return (
-        <div>
-            Services
-        </div>
+        <>
+            <section className="w-full bg-red-500 py-20 z-10">
+                <h1 data-aos="zoom-in" data-aos-delay="200" className="text-white md:text-6x1 text-5x1 font-semibold text-center pb-32">OUR SERVICES</h1>
+            </section>
+        </>
     )
 }
 

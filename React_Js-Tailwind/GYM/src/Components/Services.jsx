780664d37fa52f9825bf8465a61853ae33843c9e
jeswanth <jachu.k1212@gmail.com>
2024-11-19 08:36:25 +0530

    GYM_Working

--- React_Js-Tailwind/GYM/src/Components/Services.jsx
+++ React_Js-Tailwind/GYM/src/Components/Services.jsx
@@ -25,7 +25,8 @@ const Services = () => {
                     <div className="md:w-[85%] w-full mx-auto flex md:flex-row flex-col justify-center items-start md:gap-6 gap-20 md:p-20 p-10 mt-[200px] z-20">
                         <div data-aos="slide-up" data-aos-delay="400" className="flex flex-col justify-center items-center gap-6">
                             <img src={phil1} alt="image" width={300} height={300} />
-                            <h1>Body building</h1>
+                            <h1 className="text-4x1 text-white font-semibold">Body building</h1>
+                            <p className="text-lg text-slate-300 text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, reiciendis.</p>
                         </div>
                     </div>
 

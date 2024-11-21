06ec98320c512ca831a4c7f52f04d128b9fa4c5e
jeswanth <jachu.k1212@gmail.com>
2024-11-21 15:42:44 +0530

    GYM_Contact

--- React_Js-Tailwind/GYM/src/Components/Contact.jsx
+++ React_Js-Tailwind/GYM/src/Components/Contact.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { FaMapMarkedAlt } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+import { Iocall } from "react-icons/io5";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Contact = () => {
     return (

5d5e368edb9bff89f49f523456428c3f5a0c4f44
jeswanth <jachu.k1212@gmail.com>
2024-11-22 11:41:22 +0530

    GYM_Header

--- React_Js-Tailwind/GYM/src/Components/Header.jsx
+++ React_Js-Tailwind/GYM/src/Components/Header.jsx
@@ -1,6 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaXmark, FaBars } from "react-icons/fa6";
+import { Link } from "react-scroll";
 
 const Header = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+    }
+
+    const navItems = [
+        {
+            link: 'Home', path: 'home'
+        },
+        {
+            link: 'About', path: 'about'
+        },
+        {
+            link: 'Services', path: 'services'
+        },
+        {
+            link: 'Team', path: 'team'
+        },
+        {
+            link: 'Contact', path: 'contact'
+        }
+    ]
+    
     return (
         <div>
             Header 

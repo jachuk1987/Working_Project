9a94e1622d4b3b68b270762a29429a0f281fcdaa
jeswanth <jachu.k1212@gmail.com>
2024-11-22 11:16:56 +0530

    GYM_Footer

--- React_Js-Tailwind/GYM/src/Components/Footer.jsx
+++ React_Js-Tailwind/GYM/src/Components/Footer.jsx
@@ -7,9 +7,16 @@ const Footer = () => {
     return (
         <>
             <section className="bg-black w-full text-white p-3 flex justify-center items-center gap-4">
-            <FaCopyright className="size-6 text-white" />
-            <h1 className="text-white text-center font-semibold text-lg">Copyright 2024, Strong Zone, All Rights Reserved</h1>
+                <FaCopyright className="size-6 text-white" />
+                <h1 className="text-white text-center font-semibold text-lg">Copyright 2024, Strong Zone, All Rights Reserved</h1>
             </section>
+
+            {/* {Scroll to top button} */}
+            <div id="icon-box" className="bg-red-500 text-black p-3 rounded-full hover:bg-white cursor-pointer fixed lg:bottom-6 right-6 bottom-6" >
+                <Link to="hero" spy={true} offset={-100} smooth={true}>
+                <FaArrowUp className="w-[35px] h-[35px]" />
+                </Link>
+            </div>
         </>
     )
 }

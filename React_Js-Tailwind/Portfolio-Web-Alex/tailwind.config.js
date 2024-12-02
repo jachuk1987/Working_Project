b97dc1754794fdf022fceb5847725dc9dab0616a
jeswanth <jachu.k1212@gmail.com>
2024-12-02 20:21:32 +0530

    Finish_NewProject

---
+++ React_Js-Tailwind/Portfolio-Web-Alex/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Scans all files in the `src` folder with these extensions
+    "./public/index.html",        // Include your HTML file if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

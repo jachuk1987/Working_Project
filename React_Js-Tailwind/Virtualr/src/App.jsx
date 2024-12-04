bc8fdc1861e31a758c20460c15be7d0b749d57a1
jeswanth <jachu.k1212@gmail.com>
2024-12-04 19:58:40 +0530

    Virtualr_WorkingOnit

--- React_Js-Tailwind/Virtualr/src/App.jsx
+++ React_Js-Tailwind/Virtualr/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Navbar from "./Components/Navbar";
+import HeroSection from "./Components/HeroSection";
+import FeatureSection from "./Components/FeatureSection";
+import Workflow from "./Components/Workflow";
+import Footer from "./Components/Footer";
+import Pricing from "./Components/Pricing";
+import Testimonials from "./Components/Testimonials";
 
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <Testimonials />
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;

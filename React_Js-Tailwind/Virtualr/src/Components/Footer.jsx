be277f1fd0eb5c854a9b1c5edf835e2add58c9b1
jeswanth <jachu.k1212@gmail.com>
2024-12-04 19:43:26 +0530

    Virtular_Pricing

--- React_Js-Tailwind/Virtualr/src/Components/Footer.jsx
+++ React_Js-Tailwind/Virtualr/src/Components/Footer.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { resourcesLinks, platformLinks, communityLinks } from "../constants";
 const Footer = () => {
   return (
--- React_Js-Tailwind/Virtualr/src/Components/HeroSection.jsx
+++ React_Js-Tailwind/Virtualr/src/Components/HeroSection.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import video1 from "../assets/video1.mp4";
 import video2 from "../assets/video2.mp4";
 

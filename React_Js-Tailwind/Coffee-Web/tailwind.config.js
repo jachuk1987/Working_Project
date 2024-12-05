fabf408bb8517a17a4efef13a92c241b7c965127
jeswanth <jachu.k1212@gmail.com>
2024-12-05 11:20:54 +0530

    Coffee-Web_Components

--- React_Js-Tailwind/Coffee-Web/tailwind.config.js
+++ React_Js-Tailwind/Coffee-Web/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brightColor: "#AB6B2E",
+        backgroundColor: "#FDE9CC",
+      },
+    },
   },
   plugins: [],
-}
-
+};

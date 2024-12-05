cc4fd04cc846a7063168a79399acd345ef42167c
jeswanth <jachu.k1212@gmail.com>
2024-12-05 11:29:40 +0530

    Complete_Coffee-Web

--- React_Js-Tailwind/Coffee-Web/src/App.jsx
+++ React_Js-Tailwind/Coffee-Web/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import Menu from "./Components/Menu";
+import About from "./Components/About";
+import Product from "./Components/Product";
+import Review from "./Components/Review";
+import Footer from "./Components/Footer";
 
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Navbar />
 
-export default App
+      <main>
+        <div id="home">
+          <Home />
+        </div>
+
+        <div id="menu">
+          <Menu />
+        </div>
+
+        <div id="about">
+          <About />
+        </div>
+
+        <div id="products">
+          <Product />
+        </div>
+
+        <div id="review">
+          <Review />
+        </div>
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default App;

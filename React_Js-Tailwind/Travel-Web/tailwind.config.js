5149291a01bd104c6173971dc48702d6d4be3445
jeswanth <jachu.k1212@gmail.com>
2024-12-06 11:30:33 +0530

    Travel-Web_Pages

--- React_Js-Tailwind/Travel-Web/tailwind.config.js
+++ React_Js-Tailwind/Travel-Web/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBackground: "#0C111F",
+        brightColor: "#F9EC7D",
+      },
+    },
   },
   plugins: [],
-}
-
+};
